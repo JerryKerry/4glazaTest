b8461f580604f99d7636f68a50bbe3e7180f3920
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-08-29 16:19:59 +0300

    добавлен тест для страницы о компании

--- src/test/java/MainPageTest.java
+++ src/test/java/MainPageTest.java
@@ -1,24 +1,45 @@
-
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Configuration.baseUrl;
+import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class MainPageTest extends TestBase {
 
+    private final String registrationUrl = "https://4glaza.ru/auth/registration/";
+
     @Test
-    @DisplayName("Проверка страницы ")
-    void mainPageTest() {
-        step("Открываем страницу поиска вакансий", () ->{
-            open("https://4glaza.ru/") ;
+    @Tag("SMOKE")
+    @DisplayName("Проверка редиректа на страницу авторизации ")
+    void redirectToRegistrationPage() {
+
+        step("Открываем главную страницу", () -> {
+            open(baseUrl);
         });
 
-        step("Кликам на гео расположение", () ->{
-            mainPage.clickGeoButton();
+        step("Редирект на страницу регистрации", () -> {
+            mainPage.clickEntryButton().registrationButtonClick();
+            getUrl(registrationUrl);
         });
-        step("Кликам на каталог", () ->{
-            mainPage.clickCatalog();
+    }
+
+    @Test
+    @Tag("SMOKE")
+    @DisplayName("Проверка информации в разделе 'О магазине' на странице 'О компании'")
+    void mainPageTest() {
+
+        step("Открываем главную страницу", () -> {
+            open(baseUrl);
+        });
+
+        step("Редирект на страницу о окмпании", () -> {
+            mainPage.clickCompanyButton();
+        });
+
+        step("Проверка страницы", () -> {
+            companyPage.checkStoreText();
         });
     }
 }
