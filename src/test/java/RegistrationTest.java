68fcb985358340653aed3367712f4d742f3c0ad0
kolomiets.aa <kolomiets.aan@gazprom-neft.ru>
2025-08-29 16:20:36 +0300

    финальные правки

--- src/test/java/RegistrationTest.java
+++ src/test/java/RegistrationTest.java
@@ -1,14 +1,11 @@
-import com.codeborne.selenide.Selenide;
 import data.DataGeneration;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
 import static io.qameta.allure.Allure.step;
 
 public class RegistrationTest extends TestBase {
-
-    private String password;
 
     private DataGeneration data = new DataGeneration();
 
@@ -16,60 +13,31 @@ public class RegistrationTest extends TestBase {
     String email = data.getEmail();
     String phone = data.getPhone();
 
-    private String url = "https://4glaza.ru/auth/registration/";
-
-
     @Test
-    @DisplayName("Проверка регистрации нового пользователя")
-    public void registrationTest() {
+    @Tag("SMOKE")
+    @DisplayName("Проверка создания нового пользователя")
+    void registration() {
         String password = data.getPassword();
 
-        step("Открываем главную страницу", () ->{
-            open("https://4glaza.ru/") ;
+        step("Открываем страницу регистрации", () -> {
+            registrationPage.openPage();
         });
 
-        step("Кликаем геопозицию", () ->{
-            mainPage.clickGeoButton();
+        step("Заполняем форму данными", () -> {
+            registrationPage.setFirstLastName(firstLastName).setEmailName(email)
+                    .setPhone(phone).setPassword(String.valueOf(password)).setPasswordAgain(String.valueOf(password));
         });
 
-        step("Кликаем на иконку входа", () ->{
-            mainPage.clickEntryButton();
-        });
-
-        step("Кликаем на - 'Нет аккаунта? Зарегистрируйтесь'", () ->{
-            mainPage.registrationButtonClick();
-        });
-
-
-        step("Вводим ФИО", () ->{
-            registrationPage.setFirstLastName(firstLastName);
-        });
-
-        step("Вводим E-mail", () ->{
-            registrationPage.setEmailName(email);
-        });
-
-        step("Вводим телефон", () ->{
-            registrationPage.setPhone(phone);
-        });
-
-        step("Вводим пароль", () ->{
-            registrationPage.setPassword(String.valueOf(password));
-        });
-
-        step("Повторяем ввод пароля", () ->{
-            registrationPage.setPasswordAgain(String.valueOf(password));
-        });
-
-        step("Одобряем обработку ПДН", () ->{
+        step("Одобряем обработку ПДН", () -> {
             registrationPage.clickProcessingOfPersonalData();
         });
 
-        step("Кликаем на 'Зарегестрироваться сейчас'", () ->{
+        step("Кликаем на 'Зарегестрироваться сейчас'", () -> {
             registrationPage.clickRegistrationButton();
         });
 
-        Selenide.sleep(1000000);
-
+        step("Проверяем отображение алерта", () -> {
+            registrationPage.visibleAlertSuccess();
+        });
     }
 }
